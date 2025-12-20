2c32323addbab2af539900514a88ef0baa3d19ed
4arav-1231 <dornala.aarav@gmail.com>
2025-12-20 15:17:42 +0000

    hollow pyramid and diamond pyramid

--- Pattern/hollowpyramid.py
+++ Pattern/hollowpyramid.py
@@ -3,9 +3,8 @@
    * *
   *   *
  *     *
-*********  
+* * * * * 
 """
-
 
 
 size = int(input("choose a sizeeee: "))
@@ -13,26 +12,13 @@ size = int(input("choose a sizeeee: "))
 for rows in range(0,size):
   for space in range(size-1,rows,-1):
     print(" ",end="")
-  for star in range(0,rows+1):
+  for star in range(0,rows+1): #col
     if (rows == 0 or rows == size-1) or (star == 0 or star == rows):
-      print("*",end="")
+      print("*",end=" ")
     else:
-      print(" ",end="")
+      print(" ",end=" ")
   print()
    
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
   # if (colomn is 1st or last) or (row  is 1st or last)
     #   print "*"
 
--- homework.md
+++ homework.md
@@ -11,8 +11,20 @@
 #challenge 1 -- use to build the same using 5 loops
 #challenge 2 -- use to build the same using 3 loops
 
-    *
-   * *
-  *   *
- *     *
-*********  
+---------
+
+    * 
+   * * 
+  *   * 
+ *     * 
+* * * * * 
+    * 
+   * * 
+  *   * 
+ *     * 
+* * * * * 
+    * 
+   * * 
+  *   * 
+ *     * 
+* * * * * 
