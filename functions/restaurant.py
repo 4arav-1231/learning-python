17b10402cac7064e7e64fab3e37e9645e8b207c1
4arav-1231 <dornala.aarav@gmail.com>
2026-01-31 20:08:11 +0000

    restaurant manager

--- functions/restaurant.py
+++ functions/restaurant.py
@@ -8,45 +8,41 @@ Key Features: Add item, View item, Search item, Delete item.
 "use a list"
 
 menu = []
+
 def addItem(item):
-  menu.append(item)
+    menu.append(item)
 
 def viewItem():
-  if not menu:
-    print("Menu is empty.")
-  for i in menu:
-    print(i)
+    if not menu:
+        print("Menu is empty.")
+    for i in menu:
+        print(i)
 
 def searchItem(item):
-  if item in menu:
-    print(f"{item} is on menu.")
-  else:
-    print(f"{item} is not on menu")
+    if item in menu:
+        print(f"{item} is on menu.")
+    else:
+        print(f"{item} is not on menu")
 
 def deleteItem(item):
-  if item in menu:
-    menu.remove(item)
-  else:
-    print(f"{item} is not on menu")
-    
+    if item in menu:
+        menu.remove(item)
+    else:
+        print(f"{item} is not on menu")
+
 while True:
-  choice = input("Enter 1 to add food, 2 to view menu, 3 for searching food, 4 for deleting food, 5 to exit: ")
+    choice = input("\nEnter 1 to add food, 2 to view menu, 3 for searching food, 4 for deleting food, 5 to exit: ")
 
-  if choice == "1":
-    item = input("Enter a food: ")
-    addItem(item)
-  elif choice == "2":
-    viewItem()
-  elif choice == "3":
-    item = input("Enter a food: ")
-    searchItem(item)
-  elif choice == "4":
-    item = input("Enter a food: ")
-    deleteItem(item)
-  elif choice == "5":
-    break
-  
-
-  
-
-
+    if choice == "1":
+        item = input("Enter a food: ")
+        addItem(item)
+    elif choice == "2":
+        viewItem()
+    elif choice == "3":
+        item = input("Enter a food: ")
+        searchItem(item)
+    elif choice == "4":
+        item = input("Enter a food: ")
+        deleteItem(item)
+    elif choice == "5":
+        break
--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-import functions.search
+import functions.restaurant
